c18b4389eefbd1237178c834e6aa937bc4b79ee9
mzahita <mzahita@gmail.com>
2020-03-07 14:59:12 -0500

    Adding Country Test Verifying

--- src/test/java/CountryTest.java
+++ src/test/java/CountryTest.java
@@ -1,12 +1,37 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import io.restassured.http.ContentType;
 import model.Country;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.BaseTest;
 
+import java.util.Collection;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class CountryTest extends BaseTest {
+
+    private String apiPath = "/school-service/api/bank-accounts";
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+
+    @BeforeClass
+    public void init() {
+        MongoClient mongoClient = MongoClients.create("mongodb://techno:ee4CvCRPhor5@185.97.114.201:27118/?authSource=cloud-school");
+        database = mongoClient.getDatabase("cloud-school");
+        collection = database.getCollection("school_country");
+    }
+
+
     @Test
     public void getBasePath() {
         given()
@@ -39,6 +64,9 @@ public class CountryTest extends BaseTest {
         country.setName( name );
         country.setCode( code );
 
+        long DefaultCountry = collection.countDocuments(eq("deleted", false));
+        System.out.println(DefaultCountry);
+
         // creating country
         String countryId = given()
                 .cookies( cookies )
@@ -52,6 +80,18 @@ public class CountryTest extends BaseTest {
                 .statusCode( 201 )
                 .extract().jsonPath().getString( "id" );
 
+
+        long CreateCountry = collection.countDocuments(eq("deleted", false));
+        System.out.println(CreateCountry);
+
+        Assert.assertEquals(DefaultCountry+1,CreateCountry);
+
+        Document entity = collection.find(eq("_id",new ObjectId(countryId))).first();
+        Assert.assertNotNull( entity );
+        Assert.assertEquals( entity.get( "name" ), country.getName() );
+        Assert.assertEquals( entity.get( "code" ), country.getCode() );
+
+
         // deleting country
         given()
                 .cookies( cookies )
@@ -62,6 +102,19 @@ public class CountryTest extends BaseTest {
                 .log().body()
                 .statusCode( 200 )
         ;
+
+
+        long DeletedCountry = collection.countDocuments(eq("deleted", false));
+        System.out.println(DeletedCountry);
+        Assert.assertEquals(DefaultCountry,DeletedCountry);
+
+        Document entityAfterDeletion = collection.find(
+                and(
+                        eq( "_id", new ObjectId( countryId ) ),
+                        eq( "deleted", false )
+                )
+        ).first();
+        Assert.assertNull( entityAfterDeletion );
     }
 
     @Test
